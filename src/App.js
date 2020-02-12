3fb162c4266e908f39cbd6ae2e32f52ff5e60a13
BastienLeduc <bastien_l@hotmail.fr>
2020-02-12 18:33:25 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -1,54 +1,48 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './css/App.css';
 import './css/main.css';
-import {Route,NavLink,HashRouter} from "react-router-dom";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 import Connexion from "./pages/Connexion";
 import Menu from "./pages/Menu";
 import HistoriqueCommandes from "./pages/HistoriqueCommandes";
 import Contact from "./pages/Contact";
 
 class App extends Component {
-  render(){
-  return (
-    <HashRouter>
-    <div>
-      {/* Wrapper */}
-      <div id="wrapper">
-        {/* Header */}
-        <header id="header">
-          <div className="logo">
-            <span className="icon fa-fish" />
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          {/* Wrapper */}
+          <div id="wrapper">
+            {/* Header */}
+            <header id="header">
+              <div className="content">
+                <nav>
+                  <h1>Sushi team</h1>
+                  <ul>
+                    <li><NavLink to="/connexion">Connexion</NavLink></li>
+                    <li><NavLink to="/menu">Menu</NavLink></li>
+                    <li><NavLink to="/historiqueCommandes">Historique des commandes</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                  </ul>
+                  <Route path="/connexion" component={Connexion} />
+                  <Route path="/menu" component={Menu} />
+                  <Route path="/historiqueCommandes" component={HistoriqueCommandes} />
+                  <Route path="/contact" component={Contact} />
+                </nav>
+              </div>
+            </header>
+            {/* Footer */}
+            <footer id="footer">
+              <p className="copyright">© Alexandra Dion / Bastien Leduc / Malo Gicquel - Ei5 SAGI Polytech Angers</p>
+            </footer>
           </div>
-          <div className="content">
-            <div className="inner">
-              <h1>Sushi team</h1>
-              <p>Market</p>
-            </div>
-          </div>
-          <nav>
-            <ul>
-              <li><NavLink to="/connexion">Connexion</NavLink></li>
-              <li><NavLink to="/menu">Menu</NavLink></li>
-              <li><NavLink to="/historiqueCommandes">Historique des commandes</NavLink></li>
-              <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
-            <Route path="/connexion" component={Connexion}/>
-            <Route path="/menu" component={Menu}/>
-            <Route path="/historiqueCommandes" component={HistoriqueCommandes}/>
-            <Route path="/contact" component={Contact}/>
-          </nav>
-        </header>       
-        {/* Footer */}
-        <footer id="footer">
-          <p className="copyright">© Alexandra Dion / Bastien Leduc / Malo Gicquel - Ei5 SAGI Polytech Angers</p>
-        </footer>
-      </div>
-      {/* BG */}
-      <div id="bg" />
-      {/* Scripts */}
-    </div>
-    </HashRouter>
-  );
+          {/* BG */}
+          <div id="bg" />
+          {/* Scripts */}
+        </div>
+      </HashRouter>
+    );
   }
 }
 export default App;
