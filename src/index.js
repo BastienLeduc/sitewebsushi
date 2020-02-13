2267d698cfdc2b375722442decf5123e02812a6d
BastienLeduc <bastien_l@hotmail.fr>
2020-02-13 21:22:19 +0100

    change config

--- src/index.js
+++
@@ -1,7 +0,0 @@
-//Importer nos dépendances
-import React from 'react';
-import ReactDOM from 'react-dom';
-import'./index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
